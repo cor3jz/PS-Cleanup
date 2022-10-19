9a6fa3a995a2e160bd72cde5c22fb10a47ace3dd
Mikhail Petukhov <zuck.works@gmail.com>
2022-10-19 04:24:16 +0300

    Update Cleanup.ps1
    Файлы конфигурации, данные пользователей и кэш приложений не удаляются если их возраст меньше 24 часов

--- cleanup.ps1
+++ cleanup.ps1
@@ -13,6 +13,18 @@ function WriteLog
 }
 
 $SteamInstallPath = ''
+$SteamLibraryFolders = @()
+$SteamAppsFolders = @(
+	"downloading",
+	"shadercache",
+	"temp",
+	"workshop"
+)
+$SteamSysFolders = @(
+	"appcache",
+	"config",
+	"userdata"
+)
 
 if(Test-Path 'HKLM:\SOFTWARE\Valve\Steam') {
     $SteamInstallPath = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Valve\Steam').InstallPath
@@ -20,18 +32,7 @@ if(Test-Path 'HKLM:\SOFTWARE\Valve\Steam') {
 if(Test-Path 'HKLM:\SOFTWARE\Wow6432Node\Valve\Steam') {
     $SteamInstallPath = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Valve\Steam').InstallPath
 }
-
-$SteamClearPaths = (
-	"appcache",
-	"logs",
-	"config\*.*",
-	"steamapps\downloading",
-	"steamapps\shadercache",
-	"steamapps\sourcemods",
-	"steamapps\temp",
-	"steamapps\workshop",
-	"userdata"
-)
+if($SteamInstallPath -eq '') { throw "Can't find steam installed on this machine"}
 
 $ClearPaths = (
 	"AppData\Local\Temp",
@@ -70,6 +71,43 @@ Stop-Service "Steam Client Service" -Force | Add-Content $Logfile
 WriteLog "Steam Client Service остановлен"
 Start-Sleep -Seconds 3
 
+if(Test-Path "$SteamInstallPath\steamapps\libraryfolders.vdf") {
+	$filedata = Get-Content "$SteamInstallPath\steamapps\libraryfolders.vdf"
+
+	foreach($line in $filedata) {
+		if($line -match '"path".*"(.*)"') {
+			foreach ($SteamAppsFolder in $SteamAppsFolders)
+			{
+				$SteamLibraryFolders += "$($Matches[1])\steamapps\$SteamAppsFolder" -replace '\\\\', '\'
+			}
+		}
+	}
+}
+
+foreach ($SteamLibraryFolder in $SteamLibraryFolders)
+{
+	if ((Test-Path -Path $SteamLibraryFolder) -eq $true)
+	{
+		Get-ChildItem -Path $SteamLibraryFolder -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force -WhatIf -ErrorAction SilentlyContinue | Add-Content $Logfile
+		WriteLog "Каталог '$SteamLibraryFolder' очищен"
+	}
+}
+
+
+foreach ($SteamSysFolder in $SteamSysFolders)
+{
+	if ((Test-Path -Path "$SteamInstallPath\$SteamSysFolder") -eq $true)
+	{
+		Get-ChildItem -Path "$SteamInstallPath\$SteamSysFolder" -Recurse -Force -Exclude config.vdf, loginusers.vdf, libraryfolders.vdf, localconfig.vdf, sharedconfig.vdf | Where-Object {($_.LastWriteTime -le (Get-Date).AddHours(-24) )} | Remove-Item -Recurse -Force -WhatIf -ErrorAction SilentlyContinue | Add-Content $Logfile
+		WriteLog "Каталог '$SteamInstallPath\$SteamSysFolder' очищен"
+	}
+}
+
+if(Test-Path 'HKCU:\SOFTWARE\Valve\Steam') {
+    Get-ChildItem -Path (Get-ItemProperty -Path 'HKCU:\SOFTWARE\Valve\Steam').SourceModInstallPath | Remove-Item -Recurse -Force -WhatIf -ErrorAction SilentlyContinue | Add-Content $Logfile
+	WriteLog "Каталог 'Source Mods' очищен"
+}
+
 foreach ($Path in $ClearPaths)
 {
 	if ((Test-Path -Path "$env:userprofile\$Path") -eq $true)
@@ -79,15 +117,6 @@ foreach ($Path in $ClearPaths)
 	}
 }
 
-foreach ($SteamClearPath in $SteamClearPaths)
-{
-
-	if ((Test-Path -Path "$SteamInstallPath\$SteamClearPath") -eq $true)
-	{
-		Get-ChildItem -Path "$SteamInstallPath\$SteamClearPath" -Recurse -Force -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue | Add-Content $Logfile
-		WriteLog "'$SteamClearPath' удалено"
-	}
-}
 
 Start-Sleep -Seconds 1
 
