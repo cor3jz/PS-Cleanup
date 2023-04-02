1be03e81f0b33b87b6b2dac87c17ca89d301615f
Mikhail <zuck.works@gmail.com>
2023-04-02 03:37:19 +0300

    rewrite code

--- cleanup.ps1
+++ cleanup.ps1
@@ -2,7 +2,6 @@
 $Host.UI.RawUI.MaxPhysicalWindowSize.Width=550
 $Host.UI.RawUI.MaxPhysicalWindowSize.Height=300
 $Host.UI.RawUI.WindowTitle="Cleanup Utility" + ' - ' + $Version
-$host.UI.Write('Выполняется удаление учетных записей и настроек последнего пользователя')
 
 #Log Config
 $LogFile = $env:windir+'\'+'cleanup.log'
@@ -16,6 +15,8 @@ function WriteLog
 	$LogMessage = "$Timestamp $LogString"
 	Add-Content $LogFile -value $LogMessage
 }
+Write-Host "Cleanup Utility"
+Write-Host "Выполняется удаление учетных записей и настроек последнего пользователя"
 WriteLog "Cleanup Utility"
 WriteLog "Скрипт начал работу"
 
@@ -38,105 +39,82 @@ $Processes = (
 foreach ($Process in $Processes)
 {
 	Get-Process -name $Process -ErrorAction SilentlyContinue | ? { $_.SI -eq (Get-Process -PID $PID).SessionId } | Stop-Process -Force | Add-Content $Logfile
-	WriteLog "Работа процесса [$Process] завершена"
+
+	Write-Host "Процесс [$Process] остановлен"
+	WriteLog "Процесс [$Process] остановлен"
 }
+
 Stop-Service "Steam Client Service" -Force | Add-Content $LogFile
+
+Write-Host "Служба Steam Client Service остановлена"
 WriteLog "Служба Steam Client Service остановлена"
 
 
 #Очистка уч. записей в Steam
 $SteamPath = ''
-$SteamAppData = $env:localappdata+'\'+'Steam'
-if (Test-Path 'HKLM:\SOFTWARE\Wow6432Node\Valve\Steam') {
+if ((Test-Path 'HKLM:\SOFTWARE\Wow6432Node\Valve\Steam') -eq $true) {
     $SteamPath = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Valve\Steam').InstallPath
-}
-if ($SteamPath -eq '') {
+} else {
+	Write-Host "Не удалось найти Steam на этом компьютере"
 	WriteLog "Не удалось найти Steam на этом компьютере"
 }
-if (Test-Path $SteamAppData)
-{
-	Get-ChildItem -Path $SteamAppData | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue | Add-Content $LogFile
-	WriteLog "Кэш браузера Steam очищен"
-}
-if (Test-Path "$SteamPath\config\loginusers.vdf")
+
+if ((Test-Path "$SteamPath\config\loginusers.vdf") -eq $true)
 {
 	Remove-Item "$SteamPath\config\loginusers.vdf" -Force -ErrorAction SilentlyContinue | Add-Content $LogFile
-	WriteLog "Данные учетных записей Steam удалены"
+	Write-Host "Учетные данные Steam удалены"
+	WriteLog "Учетные данные Steam удалены"
+} else {
+	Write-Host "Учетные данные Steam не обнаружены"
+	WriteLog "Учетные данные Steam не обнаружены"
 }
 Start-Sleep -Seconds 1
 
-
-#Очистка уч. записей в EA App
-if (Test-Path "$env:localappdata\Electronic Arts\EA Desktop\*.ini")
-{
-	Remove-Item "$env:localappdata\Electronic Arts\EA Desktop\*.ini" -Force -ErrorAction SilentlyContinue | Add-Content $LogFile
-	WriteLog "Данные учетных записей EA Desktop удалены"
-}
-Start-Sleep -Seconds 1
-
-
-#Очистка уч. записей в VK Play
-if (Test-Path "$env:localappdata\GameCenter\GameCenter.ini")
-{
-	Remove-Item "$env:localappdata\GameCenter\GameCenter.ini" -Force -ErrorAction SilentlyContinue | Add-Content $LogFile
-	WriteLog "Данные учетных записей VK Play удалены"
-}
-Start-Sleep -Seconds 1
-
-
-#Очистка уч. записей в Epic Games
-if (Test-Path "$env:localappdata\EpicGamesLauncher\Saved\Config\Windows\GameUserSettings.ini")
-{
-	Remove-Item "$env:localappdata\EpicGamesLauncher\Saved\Config\Windows\GameUserSettings.ini" -Force -ErrorAction SilentlyContinue | Add-Content $LogFile
-	WriteLog "Данные учетных записей Epic Games удалены"
-}
-Start-Sleep -Seconds 1
-
-
-#Очистка уч. записей в Battle.net
-if (Test-Path -Path "$env:appdata\Battle.net")
-{
-	Get-ChildItem -Path "$env:appdata\Battle.net" | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue | Add-Content $LogFile
-	WriteLog "Данные учетных записей Battle.net удалены"
-}
-Start-Sleep -Seconds 1
-
-
-#Очистка уч. записей в Lesta Games
-if (Test-Path "$env:localappdata\Lesta\GameCenter\user_info.xml")
-{
-	Remove-Item "$env:localappdata\Lesta\GameCenter\user_info.xml" -Force -ErrorAction SilentlyContinue | Add-Content $LogFile
-	WriteLog "Данные учетных записей Lesta Games удалены"
-}
-Start-Sleep -Seconds 1
-
-
-#Очистка уч. записей в Wargaming.net
-if (Test-Path "$env:localappdata\Wargaming.net\GameCenter\user_info.xml")
-{
-	Remove-Item "$env:localappdata\Wargaming.net\GameCenter\user_info.xml" -Force -ErrorAction SilentlyContinue | Add-Content $LogFile
-	WriteLog "Данные учетных записей Wargaming.net удалены"
-}
-Start-Sleep -Seconds 1
-
-
-#Удаление профилей в Chrome, Discord и FaceIT
-$AppPaths = (
-	"$env:localappdata\Google\Chrome\User Data",
-	"$env:appdata\discord",
-	"$env:appdata\FACEIT"
+#Удаление учетных данных других приложений
+$CredentialStores = (
+	"$env:localappdata\Electronic Arts\EA Desktop\cookie.ini",
+    "$env:localappdata\GameCenter\GameCenter.ini",
+    "$env:localappdata\EpicGamesLauncher\Saved\Config\Windows\GameUserSettings.ini",
+    "$env:localappdata\Steam\*",
+    "$env:appdata\Lesta\GameCenter\user_info.xml",
+    "$env:appdata\Wargaming.net\GameCenter\user_info.xml",
+    "$env:appdata\Battle.net\*.config",
+    "$env:localappdata\Google\Chrome\User Data\*",
+	"$env:appdata\discord\*",
+	"$env:appdata\FACEIT\*"
 )
-foreach ($AppPath in $AppPaths)
+
+foreach ($CredentialFile in $CredentialStores)
 {
-	if (Test-Path -Path $AppPath)
-	{
-		Get-ChildItem -Path $AppPath | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue | Add-Content $LogFile
-		WriteLog "Каталог [$AppPath] очищен"
-	}
+    $FileName = $CredentialFile.Split("\")[5]
+
+    switch ($FileName)
+    {
+        'Battle.net' {$Message = 'Учетные данные Battle.net удалены'}
+        'Electronic Arts' {$Message = 'Учетные данные EA Desktop удалены'}
+        'GameCenter' {$Message = 'Учетные данные VK Play удалены'}
+        'EpicGamesLauncher' {$Message = 'Учетные данные Epic Games удалены'}
+        'Lesta' {$Message = 'Учетные данные Lesta Games удалены'}
+        'Steam' {$Message = 'Кэш браузера Steam очищен'}
+        'Wargaming.net' {$Message = 'Учетные данные Wargaming.net удалены'}
+        'Google' {$Message = 'Профиль пользователя Google Chrome удален'}
+        'discord' {$Message = 'Учетные данные Discord удалены'}
+        'FACEIT' {$Message = 'Учетные данные FACEIT удалены'}
+    }
+
+    if ((Test-Path "$CredentialFile") -eq $true) {
+        Get-Item $CredentialFile | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue | Add-Content $Logfile
+        WriteLog "$Message"
+	    Write-Host "$Message"
+    } else {
+        WriteLog "Данные $FileName не обнаружены"
+	    Write-Host "Данные $FileName не обнаружены"
+    }
 }
 
+Start-Sleep -Seconds 1
 
-#Удаление временных файлов и очистка каталогов пользователя Windows
+#Очистка системных папок и каталогов пользователя
 $SystemPaths = (
 	"$env:localappdata\Temp",
 	"$env:localappdata\CrashDumps",
@@ -145,17 +123,42 @@ $SystemPaths = (
 	"$env:userprofile\Videos",
 	"$env:userprofile\Music"
 )
-foreach ($SysPath in $SystemPaths)
+foreach ($Path in $SystemPaths)
 {
-	if (Test-Path -Path $)
-	{
-		Get-ChildItem -Path $SysPath | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue | Add-Content $LogFile
-		WriteLog "Каталог [$SysPath] очищен"
-	}
-}
-WriteLog "Каталоги пользователя и временные файлы системы очищены"
+    $DirName = $Path.Split("\")[-1]
 
-#Очистка корзины
-Clear-RecycleBin -Force
+    switch ($DirName)
+    {
+        'Temp' {$Message = 'Временные файлы Windows удалены'}
+        'CrashDumps' {$Message = 'Дампы крашей удалены'}
+        'Downloads' {$Message = 'Папка Загрузки очищена'}
+        'Pictures' {$Message = 'Папка Изображения очищена'}
+        'Videos' {$Message = 'Папка Видео очищена'}
+        'Music' {$Message = 'Папка Музыка очищена'}
+    }
+
+    if ((Test-Path "$Path") -eq '') {
+        WriteLog "Каталог [$Path] не существует"
+	    Write-Host "Каталог [$Path] не существует"
+        continue
+    }
+
+    if ((Test-Path "$Path\*") -eq '') {
+        WriteLog "Каталог $DirName пуст"
+	    Write-Host "Каталог $DirName пуст"
+        continue
+    }
+
+    Get-ChildItem -Path $Path | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue | Add-Content $LogFile
+    WriteLog "$Message"
+	Write-Host "$Message"
+}
+
+Start-Sleep -Seconds 1
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue | Add-Content $LogFile
 WriteLog "Корзина очищена"
 WriteLog "Скрипт завершил свою работу"
+
+Write-Host "Корзина очищена"
+Write-Host "Скрипт завершил свою работу"
